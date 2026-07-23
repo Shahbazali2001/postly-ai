09d8aace1f2fca9ef2b948f7d9809af16fc566f2
Shahbazali2001 <Shahbazali5097855@gmail.com>
2026-07-23 11:58:35 +0500

    added models

---
+++ backend/models/Account.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { platform } from "node:os";
+
+const accountSchema = new mongoose.Schema({
+    user : {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
+    platform : {type: String, enum: ["instagram", "linkedin", "twitter", "facebook" ,"facebook_page", "linkedin_page", "instagram_business"], required: true},
+    handle : {type: String, required: true},
+    zernioAccountId : {type: String},
+    accessToken : {type: String},
+    refreshToken : {type: String},
+    tokenExpiresAt: {type: Date},
+    status : {type: String, enum: ["connected", "disconnected"], default: "connected"},
+    avatarUrl : {type: String},
+}, {
+    timestamps: true
+});
+
+export const Account = mongoose.model("Account", accountSchema);
