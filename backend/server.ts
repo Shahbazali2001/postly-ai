fbe28271d216b1a72c36d34d29c15a253e96c0a8
Shahbazali2001 <Shahbazali5097855@gmail.com>
2026-07-29 11:38:06 +0500

    working on server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 
 // DB Connection and PortConfig Import
@@ -13,15 +13,15 @@ import accountRouter from "./routes/accountRoutes.js";
 import postRouter from "./routes/postRoutes.js";
 import activityRouter from "./routes/activityRoutes.js";
 
-
+// Services Import
+import { initScheduler } from "./services/schedulerService.js";
 
 // Express App Variables
 const app = express();
 
 // Using Middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-
 
 // Auth Routes
 app.use("/api/auth", authRouter);
@@ -38,12 +38,13 @@ app.use("/api/posts", postRouter);
 // Activity Route
 app.use("/api/activity", activityRouter);
 
-
-
 // Home Route
-app.get('/', (_req: Request, res: Response) => {
-    res.send('Server is Live!');
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Server is Live!");
 });
+
+// Scheduler
+initScheduler();
 
 
 // Error Handler
@@ -55,13 +56,12 @@ app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
   });
 });
 
-
-
 // Start Server and Connect DB
 connectDB().then(() => {
-    app.listen(config.port, () => {
-        console.log
-        console.log(`Server is started and running at http://localhost:${config.port}`);
-    });
-})
-
+  app.listen(config.port, () => {
+    console.log;
+    console.log(
+      `Server is started and running at http://localhost:${config.port}`,
+    );
+  });
+});
